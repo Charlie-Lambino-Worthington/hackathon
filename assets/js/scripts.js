8ff1dd83d1a6edcc1aa1a0ca2563841fe557cb50
Charlie Lambino-Worthington <charlie.m.k.worthington@gmail.com>
2024-05-28 16:40:36 +0000

    check if answers are correct and apply class to reflect this

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -208,5 +208,18 @@ const loadQuiz = () => {
   acceptingAnswers = true;
 };
 
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+      if (!acceptingAnswers) return;
   
+      acceptingAnswers = false;
+      const selectedChoice = e.target;
+      const selectedAnswer = selectedChoice.dataset['number'];
+  
+      const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+      if (classToApply === "correct") {
+        incrementScore(CORRECT_BONUS);
+      }
+  
+      selectedChoice.parentElement.classList.add(classToApply);
   
